8ca814c29359542b95fe8c7a55c560f4b21cd9e8
Karla Lira Rangel <karla.lira@udem.edu>
2021-09-02 22:41:56 -0500

    pantalla inicio

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 //import App from './src/App';
 import Login from './src/Login';
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => Login);
+SplashScreen.hide();
 
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import Header from './Header';
+import {Input, Button} from 'react-native-elements';
+//import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   
   Text, StyleSheet, Image, View, StatusBar
@@ -10,42 +12,63 @@ import LinearGradient from 'react-native-linear-gradient';
 const Login =() =>{
   
   return (
-    <View style={styles.header}>   
-    <StatusBar
-    animated={true}
-    backgroundColor="#EAC56E"
-    barStyle={'dark-content'}
-    showHideTransition={'none'}
-    hidden={false} />
-    <Text style={styles.headerTitulo}>Iniciar Sesión</Text>
+    <View style={styles.container}>   
+      <Header title={"Iniciar Sesión"}> </Header>
+      <View style={styles.innerContainer}>
+        <Input containerStyle={styles.input} labelStyle={styles.label}
+          label="Correo"
+          placeholder='user@email.com'
+        />
+        <Input secureTextEntry={true}  labelStyle={styles.label}  containerStyle={styles.input}
+          label="Contraseña"
+          placeholder='********'
+        />
+         <Button  buttonStyle={styles.button} titleStyle={styles.buttonTitle}
+          title="Iniciar Sesión"
+        />
+        <Text style={styles.text}>
+           <Text > ¿Aún no tienes cuenta? </Text>
+           <Text style={styles.regis}> Regístrate </Text>
+        </Text>
+      </View>
     </View>
   );
 } 
 // estilo =css, es un objeto js
 var styles = {
-  header:{
-      backgroundColor: "#EAC56E",
-      height: 100,
-      borderBottomLeftRadius: 20,
-      borderBottomRightRadius: 20,
-      shadowColor: "#000000",
-    shadowOffset: {
-        width: 0,
-        height: 9,
-    },
-    shadowOpacity: 1.0,
-    shadowRadius: 1.84,
-    elevation: 10,
+  container:{
+      flex:1
   },
-  headerTitulo: {
-    fontSize: 34,
-    fontFamily: 'Gill Sans',
-    textAlign: 'center',
-    margin: 10,
-    color: '#000000',
-    fontWeight:"bold"
+  innerContainer:{
+    flex:1,
+    padding:40,
   },
- 
+  input:{
+    marginBottom:30,
+    marginTop:30,
+  },
+  label:{
+    fontSize:24,
+    color: "#000000",
+  },
+  button:{
+    backgroundColor:"#FFD46F",
+    borderRadius:20,
+    marginTop:20,
+  },
+  buttonTitle:{
+    color:"#000000",
+    fontSize:18,
+  },
+  text:{
+    color:"#000000",
+    fontSize:18,
+    marginTop:100,
+  },
+  regis:{
+    color:"#C88037",
+    textDecorationLine: 'underline'
+  }
 };
 
 export default Login;

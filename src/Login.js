364560746269a8352e33cf9406264c9c616c950b
Karla Lira Rangel <karla.lira@udem.edu>
2021-10-25 22:17:52 -0500

    sigue funcionando

--- src/Login.js
+++ src/Login.js
@@ -2,26 +2,17 @@ import React, { useState } from 'react';
 import Header from './Header';
 import {Input, Button} from 'react-native-elements';
 //import Icon from 'react-native-vector-icons/FontAwesome';
-import {
-  
-  Text, StyleSheet, Image, View, StatusBar
-} from 'react-native';
-
+import {Text, StyleSheet, Image, View, StatusBar, KeyboardAvoidingView,AsyncStorage} from 'react-native';
+import axios from "axios";
 //para el color gradiente
 import LinearGradient from 'react-native-linear-gradient';
-const loginBack=(correo, contrasena) =>{
-  return new Promise((resolve, reject)=>{
-    //simula back
-    setTimeout(()=>{
-      resolve(false)
-    }, 2000)
-  })
-}
+
 const Login =({navigation}) =>{
   const [loading,setLoading] = useState(false);
   const [correo,setCorreo] = useState(false);
   const [contrasena,setContrasena] = useState(false);
   const [errorMessage, setErrorMessage] =useState("");
+  const [errorType, setErrorType] =useState("");
 
   const loginPressed=()=>{
     setLoading(true);
@@ -31,13 +22,31 @@ const Login =({navigation}) =>{
       setLoading(false)
       return
     }
-    loginBack(correo, contrasena).then(result=>{
-      navigation.navigate("Location")
-      setLoading(false)
-      setErrorMessage("Invalid Credential")
-    })
-  }
-
+    else {
+      //SIMULACIÓN DEBERIA SER AXIOS
+          setLoading(true);
+          axios.post("http://10.0.2.2:3001/user/login",{
+            correo: correo, contrasenia:contrasena
+          }).then(async(res)=>{
+            console.log(res.status)
+            setLoading(false);
+            navigation.navigate("Location")
+            //libreria para guardar info del usuario
+            await AsyncStorage.setItem("user", JSON.stringify(res.data.login))
+          
+          })
+          .catch(error=>{
+            console.log(error.response)
+            if(error.response.status===400){
+              setErrorMessage("Correo o contraseña incorrectos")
+              setErrorType("reapited")
+              console.log(error.response.status)
+              setLoading(false);
+            }
+         })
+        }
+    }
+  
   const validateEmail=(email) =>{
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -63,6 +72,7 @@ const Login =({navigation}) =>{
           label="Contraseña"
           placeholder='********'
         />
+         <Text>{errorType==="reapited"?errorMessage:""} </Text>
          <Button loading={loading}  buttonStyle={styles.button} titleStyle={styles.buttonTitle} onPress={loginPressed}
           title="Iniciar Sesión"
         />
